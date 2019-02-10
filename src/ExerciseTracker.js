4e344a6df631c6433eed8b93bc7dd94c46f8caf8
Paige Lowe <paigelowee@aol.com>
2019-02-10 14:09:23 +0000

    totalcalsnotworking

--- src/ExerciseTracker.js
+++ src/ExerciseTracker.js
@@ -1,57 +1,88 @@
 import React from 'react';
 import Workout from './Workout';
-
+import moment from 'moment'
+import uuid from 'uuid'
+import './App.css';
 
 class ExerciseTracker extends React.Component {
 
-    constructor(props) {
-   		super(props);
-		this.state = { workouts: [], newWorkout: '' };
-	}
-	//method 
-	updateNewWorkout = (event) => {
-  		this.setState({ newWorkout: event.target.value });
-	}
-	//mehthod to add workouts
+  constructor(props) {
+    super(props);
+		this.state = { workouts: [],
+                   newWorkout: '',
+                  newcalories:'',
+                  totalcalories:0,
+                   };
+  }
+
+  updateNewWorkout = (event) => {
+  	 this.setState({ newWorkout: event.target.value });
+	   }
+
+  updatenewcalorie = (event) => {
+     this.setState({ newcalories: event.target.value });
+    }
+
 	addWorkout = () => {
-		//create  new workout obj
+    const id = uuid.v4();
   		const workout = {
-	  		//title got from state date built in java
 	    	title: this.state.newWorkout,
-	    	date: new Date()
+	    	id:id,
+        cals: this.state.newcalories,
+        time: moment().format('MMMM Do YYYY, h:mm:ss a')
   		};
-  		//form array
-		const workouts = Object.assign([], this.state.workouts);
-  		//push workout onto array (save it)
+
+		  const workouts = Object.assign([], this.state.workouts);
   		workouts.push(workout);
-  		//update state
-  		this.setState({ workouts });
+
+      let newcal = this.state.newcalories + this.state.totalcalories;
+      this.setState({totalcalories:newcal});
+
+  		this.setState({ workouts });	
+      this.setState({ newWorkout: '' });
+      this.setState({ newcalories: '' });
 	}
 
-	//method delete workouts
 	deleteWorkout = (workout) => {
   		const workouts = Object.assign([], this.state.workouts);
   		//locate workout to delete (iterate through array)
   		for (let i = 0; i < workouts.length; i++) {
-    		if (workout.date.getTime() === workouts[i].date.getTime()) {
+    		if (workout.id === workouts[i].id ){
       			//remove the element at pos i (workout to be deleted)
       			workouts.splice(i, 1);
     		}
   		}
+      const newcal = this.state.totalcalories - this.state.newcalories;
+      this.setState({totalcalories:newcal});
   		//update the state
   		this.setState({ workouts });
 	}
 
-  	render() {
+  render() {
     	return (
-      		<div>
-				<input placeholder='Add a new workout' value={this.state.newWorkout} onChange={this.updateNewWorkout} />
-				<button onClick={this.addWorkout}>Add workout</button>
-				<ul>
-  					{this.state.workouts.map(workout => 
-					<Workout workout={workout} key={workout.date} deleteWorkout={this.deleteWorkout} />
-  					)}
-				</ul>
+        <div>
+          <div class='addworkout'>
+            <label1>Workout name</label1>
+            <br/>
+  				  <input  value={this.state.newWorkout} onChange={this.updateNewWorkout} />
+            <br/>
+            <label1>Calories burned</label1>
+            <br/>
+            <input type="number" value={this.state.newcalories} onChange={this.updatenewcalorie} />
+            <br/>
+            <br/>
+  				  <button class='button' onClick={this.addWorkout}>Save workout</button>
+  			</div>
+          <div class='workouts'>
+            <h3>Workouts</h3>
+          <ul>
+    			   {this.state.workouts.map(workout => 
+  					 <Workout workout={workout} key={workout.date} deleteWorkout={this.deleteWorkout} />
+    					)}
+  				</ul>
+          <p> Total calories burnt: {this.state.totalcalories} </p>
+          </div>
+
       	</div>
 
     	);
--- src/Workout.js
+++ src/Workout.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import './App.css';
 
 class Workout extends React.Component {
   render() {
     return (
     	<div>
-	    	<li>{this.props.workout.title}  
-	    	<button onClick={() => this.props.deleteWorkout(this.props.workout)}>Delete</button>
+	    	<li>Workout: {this.props.workout.title}<br/>
+	    		Calories burned: {this.props.workout.cals}<br/>
+	    		Date/Time: {this.props.workout.time}<br/><br/>
+	    	<button class='button' onClick={() => this.props.deleteWorkout(this.props.workout)}>Delete workout</button>
 			</li>
     	</div>
     );
